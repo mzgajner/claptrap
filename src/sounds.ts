64e4b25d1f8b174f7912e8dd9180488c5dc83e1e
mzgajner <mato@zgajner.com>
2023-07-22 00:56:38 +0100

    Add a bunch more sounds

--- src/sounds.ts
+++ src/sounds.ts
@@ -13,5 +13,115 @@ export default [
 		path: 'sounds/murloc.mp3',
 		title: 'Murloc',
 		subtitle: 'World of Warcraft'
-	}
+	},
+	{
+		path: 'sounds/leroy-jenkins.mp3',
+		title: 'Leroy Jenkins',
+		subtitle: 'World of Warcraft'
+	},
+	{
+		path: 'sounds/work-work.mp3',
+		title: 'Work, work',
+		subtitle: 'Warcraft 3'
+	},
+	{
+		path: 'sounds/gta-san-andreas-mission-complete.mp3',
+		title: 'Mission Complete',
+		subtitle: 'GTA: San Andreas'
+	},
+	{
+		path: 'sounds/for-mother-russia.mp3',
+		title: 'For Mother Russia',
+		subtitle: 'Red Alert 2'
+	},
+	{
+		path: 'sounds/reinforcements-ready.mp3',
+		title: 'Reinforcements ready',
+		subtitle: 'Red Alert 2'
+	},
+	{
+		path: 'sounds/pacman-wakawaka.mp3',
+		title: 'Wakawaka',
+		subtitle: 'Pac-Man'
+	},
+	{
+		path: 'sounds/pacman-game-over.mp3',
+		title: 'Game Over',
+		subtitle: 'Pac-Man'
+	},
+	{
+		path: 'sounds/construct-pylons.mp3',
+		title: 'Construct Pylons',
+		subtitle: 'Starcraft'
+	},
+	{
+		path: 'sounds/terrorists-win.mp3',
+		title: 'Terrorists Win',
+		subtitle: 'Counter-Strike'
+	},
+	{
+		path: 'sounds/enemy-flag.mp3',
+		title: 'The enemy has your flag',
+		subtitle: 'Quake 3 Arena'
+	},
+	{
+		path: 'sounds/hadouken.mp3',
+		title: 'Hadouken',
+		subtitle: 'Street Fighter 2'
+	},
+	{
+		path: 'sounds/shoryuken.mp3',
+		title: 'Shoryuken',
+		subtitle: 'Street Fighter 2'
+	},
+	{
+		path: 'sounds/hail-to-the-king.mp3',
+		title: 'Hail to the king, baby',
+		subtitle: 'Duke Nukem 3D'
+	},
+	{
+		path: 'sounds/time-to-kick-ass.mp3',
+		title: 'It’s time to kick ass',
+		subtitle: 'Duke Nukem 3D'
+	},
+	{
+		path: 'sounds/spotted.mp3',
+		title: '!',
+		subtitle: 'Metal Gear Solid'
+	},
+	{
+		path: 'sounds/mario-coin.mp3',
+		title: 'Coin',
+		subtitle: 'Super Mario Bros.'
+	},
+	{
+		path: 'sounds/mario-jump.mp3',
+		title: 'Jump',
+		subtitle: 'Super Mario Bros.'
+	},
+	{
+		path: 'sounds/monsterkill.mp3',
+		title: 'Monsterkill',
+		subtitle: 'Unreal Tournament'
+	},
+	{
+		path: 'sounds/ea-sports.mp3',
+		title: 'It’s in the game',
+		subtitle: 'EA Sports'
+	},
+	{
+		path: 'sounds/finish-him.mp3',
+		title: 'Finish him',
+		subtitle: 'Mortal Kombat'
+	},
+	{
+		path: 'sounds/fatality.mp3',
+		title: 'Fatality',
+		subtitle: 'Mortal Kombat'
+	},
+	{
+		path: 'sounds/desa-reakcije.mp3',
+		title: 'Jst mam reakcije, morm reč, kr dobre',
+		subtitle: 'Desa Muck'
+	},
 ]
