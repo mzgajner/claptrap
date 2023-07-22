b1ab1478551b71504b81bfc61fefa05595ec5849
mzgajner <mato@zgajner.com>
2023-07-22 16:01:35 +0100

    Simplify grid behaviour

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,56 +8,20 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class="wrapper">
-  <section>
-    {#each sounds as sound}
-      <SoundButton {...sound} />
-    {/each}
-  </section>
-</div>
+<section>
+  {#each sounds as sound}
+    <SoundButton {...sound} />
+  {/each}
+</section>
 
 <style>
-  .wrapper {
-    display: flex;
-    justify-content: center;
-    min-height: 100vh;
-  }
-
   section {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr) );
     grid-auto-rows: 1fr;
     grid-gap: 1rem;
     justify-content: space-between;
     padding: 1rem 1rem;
     box-sizing: border-box;
-    max-width: 1140px;
-    width: 100%
-  }
-
-  @media (min-width: 1140px) {
-    .wrapper {
-      align-items: center;
-    }
-  }
-  @media (max-width: 1139px) {
-    section {
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-    }
-  }
-  @media (max-width: 906px) {
-    section {
-      grid-template-columns: 1fr 1fr 1fr;
-    }
-  }
-  @media (max-width: 690px) {
-    section {
-      grid-template-columns: 1fr 1fr;
-    }
-  }
-  @media (max-width: 480px) {
-    section {
-      grid-template-columns: 1fr;
-    }
   }
 </style>
